484daa3d0d3b3d7c0ccafea5c72290277d3a4f62
tamiresaraujoo@hotmail.com <tamiresaraujoo@hotmail.com>
2023-07-03 13:40:11 -0300

    Aula

---
+++ aula-030723/scriptPage.js
@@ -0,0 +1,1 @@
+alert ("Executando Js")
---
+++ aula-030723/variaveis.js
@@ -0,0 +1,7 @@
+// Java -> Linguagem de Programação (compilada)
+// JavaScript -> Linguagem de de Programação (Interpretada)
+
+/* Comentário de mutilplas linhas */
+// Comentário de única linha
+console.log ("Texto")
+console.log (5)
