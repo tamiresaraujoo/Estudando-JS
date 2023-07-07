7b52720a266c199bc3169ce8903060a0a15c3a72
tamiresaraujoo@hotmail.com <tamiresaraujoo@hotmail.com>
2023-07-07 14:17:13 -0300

    aula de hoje

---
+++ aula-070723/atividade.js
@@ -0,0 +1,6 @@
+//Escreva um programa que imprima os números de 1 a 20 usando um loop for.
+for(let i = 1; i <=20; i++)
+console.log(i);
+
+
+//Escreva um programa que exiba os números de Fibonacci até o décimo termo. Os números de Fibonacci são formados pela sequência 0, 1, 1, 2, 3, 5, 8, 13, ... em que cada número é a soma dos dois anteriores. Use um loop while para gerar a sequência.
